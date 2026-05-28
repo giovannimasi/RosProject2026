ea647c09372b4aaeed3aa033b1255f76e282e7c4
giovannimasi03 <giovannimasi03@gmail.com>
2026-05-28 15:06:51 +0200

    feat: first algoritm for testing commands and lidar

---
+++ ros1_ws/build/gt_motion_planner/CMakeFiles/gt_planner_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/root/ros1_ws/devel/.private/gt_motion_planner/lib/gt_motion_planner/gt_planner_node"
+  "/root/ros1_ws/devel/.private/gt_motion_planner/lib/gt_motion_planner/gt_planner_node.pdb"
+  "CMakeFiles/gt_planner_node.dir/src/gt_planner_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gt_planner_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
